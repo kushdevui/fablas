37e4b9111a6c9e6e5b090df5ab1f9b0408f9d57d
anshulGrover <anshul1601.grover@gmail.com>
2019-01-26 19:43:03 +0530

    product and user updated

--- server/users/users.controller.js
+++ server/users/users.controller.js
@@ -24,6 +24,7 @@ router.get('/' , ( req , res , next ) => {
  * @todo Extraction should only be possible only if User is Authenticated
  */
 router.get('/extract/:id' , (req , res , next) => {
+    
     const user_id = req.params.id;  
 
     userServices.getSingleUser( user_id )
@@ -51,9 +52,7 @@ router.post( '/newUser' , (req , res , next) => {
             user_middle_name,
             user_last_name,
             user_phone,
-            user_password,
-            user_type,
-            optedForNewsletter
+            user_password
         } = req.body;
 
         userServices.saveNewUser({
@@ -64,12 +63,11 @@ router.post( '/newUser' , (req , res , next) => {
                 user_middle_name,
                 user_last_name
             },
-            user_type,
             user_phone_numbers : [user_phone],
-            user_password,
-            optedForNewsletter
+            user_password
         })
         .then( (data) => {
+            res.setHeader("x-auth",data.tokens[0].token);
             res.json(data);
         })
         .catch( (err) => {
@@ -78,7 +76,7 @@ router.post( '/newUser' , (req , res , next) => {
     } else {
         res.send("Header not Set");
     }
-})
+});
 
 // Exporting the module
 export default router;
