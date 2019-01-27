ce813af56bdd62d09ad11f2fe9cea7019a37220c
anshulGrover <anshul1601.grover@gmail.com>
2019-01-27 08:21:11 +0530

    productUpdated

--- server/product/product.models.js
+++ server/product/product.models.js
@@ -1,32 +1,81 @@
 import mongoose from "mongoose";
 const product_schema=mongoose.Schema({
-	id:{
-		type:String,
-		required: [true , "Cannot Be Blank"],
-		unique:true,
-		index:true
-	},
-	name:{
-		type:String,
-		required:true,
-	},
-	category:{
-		type:String,
-		required:false
-	},
-	price:{
-		type:Number,
-		required: [true, "Product should have a price!"]
-	},
-	image_path:{
-		type:String,
-		required:[true, "Product should have a price!"]
-	},
-	isFeatured:{
-		type:Boolean,
-		required:true,
-		default:false
-	}
+			categoryName:{
+				type:String,
+				required:true
+			},
+			categoryId:{
+				type:String,
+				required:true
+			},
+			subCategory:[{
+				name:{
+					type:String,
+					required:true
+				},
+				id:{
+					type:String,
+					required:true
+				},
+				productsList:[
+					{
+						name:{
+							type:String,
+							required:true
+						},
+						imagepath:{
+							type:String,
+							required:true
+						},
+						isFeatured:{
+							type:Boolean,
+							required:true,
+							default:false
+						},
+						price:{
+							type:String,
+							required:true
+						}
+					}
+				]
+			}]
+			
+		
+	
+
+	// id:{
+	// 	type:String,
+	// 	required: [true , "Cannot Be Blank"],
+	// 	unique:true,
+	// 	index:true
+	// },
+	// name:{
+	// 	type:String,
+	// 	required:true,
+	// },
+	// category:{
+	// 	name:{
+	// 		type:String,
+	// 		required:true
+	// 	},
+	// 	subCategory:{
+	// 		type:String,
+	// 		required:false
+	// 	}
+	// },
+	// price:{
+	// 	type:Number,
+	// 	required: [true, "Product should have a price!"]
+	// },
+	// image_path:{
+	// 	type:String,
+	// 	required:[true, "Product should have a price!"]
+	// },
+	// isFeatured:{
+	// 	type:Boolean,
+	// 	required:true,
+	// 	default:false
+	// }
     
 });
 export default mongoose.model("products" , product_schema);
