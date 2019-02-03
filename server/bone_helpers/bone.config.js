8a7a5b4685fa3f8758371e9f1362564afd8b7eea
anshulGrover <anshul1601.grover@gmail.com>
2019-02-03 19:47:43 +0530

    database updated

--- server/bone_helpers/bone.config.js
+++ server/bone_helpers/bone.config.js
@@ -9,7 +9,7 @@ const DB_PORT = 27017;
 
 
 export default {
-    "connectionString" : `mongodb://${DB_HOSTNAME} : ${DB_PORT}/${database_name}`,
+    "connectionString" : `mongodb://anshul1601:anshul123#@ds121105.mlab.com:21105/fablas`,
     "successMsg" : "Database Connection SUCCESS!!!",
     "errorMsg" : "MONGO CONNECTION ERROR!!",
     "secret" : "BONY BONY FLUFF. PEOPLE CRY TO BUTTS. THIS IS MY SECRET. BLessed Old Gods. MINGLE mingle.... !!!!!! TO TO TOT TOTO x0x0 366 leapy!!"
