04bcfc6924e06bc5222164d7cf48bc0d25de5884
shreya krishnan <kushdevui@gmail.com>
2019-04-08 21:23:43 +0530

    inner pages responsive

--- src/container/SectionHeading/sectionHeading.js
+++ src/container/SectionHeading/sectionHeading.js
@@ -5,7 +5,7 @@ const SECTIONHEADING = (props) =>{
     return(
         <div className="row text-center section-head-tile">
             <h1 className="col-lg-12">{props.title}</h1>
-            <p className="col-lg-12">{props.subTitle}</p>
+            <p className="col-lg-12 m-0">{props.subTitle}</p>
         </div>
     )
 }
