5d098dc4856423a02c2aafb32cfb809ef46751a7
Kush Kumar <kuskumar0@publicisgroupe.net>
2019-04-09 18:18:32 +0530

    submit form on contact page

--- src/container/EnquiryForm/enquiryForm.js
+++ src/container/EnquiryForm/enquiryForm.js
@@ -1,30 +1,60 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const EnquiryForm = () =>{
-    return(
-        <div>
-            <div className="row">
-                <div className="col-lg-6">
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Your Name"/>
+class EnquiryForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            errorMsg:"",
+            contactForm:{
+                fullName:"",
+                email:"",
+                message:""
+            }
+        }
+        this.handleChangeForm = this.handleChangeForm.bind(this);
+    }
+
+    handleContactSubmit = (e) =>{
+        e.preventDefault();
+
+    }
+
+    handleChangeForm(event){
+        var name = event.target.name;
+        var value = event.target.value;
+        
+    }
+
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleContactSubmit}>
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="form-group">
+                                <input type="text" onChange={this.handleChangeForm} name="fullName" className="form-control" placeholder="Your Name"/>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="form-group">
+                                <input type="text" onChange={this.handleChangeForm} name="email" className="form-control" placeholder="Your Email"/>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="col-lg-6">
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Your Email"/>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="form-group">
+                                <textarea onChange={this.handleChangeForm} name="message" className="form-control" placeholder="Message"></textarea>
+                            </div>
+
+                            <button className="btn btn-danger form-control ">Send Your Message</button>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
-            <div className="row">
-                <div className="col-lg-12">
-                    <div className="form-group">
-                        <textarea className="form-control" placeholder="Message"></textarea>
-                    </div>
-                    <button className="btn btn-danger form-control ">Send Your Message</button>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default EnquiryForm;
