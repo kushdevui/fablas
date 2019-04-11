9d50c293cd36bcdce4dc135683b05509c8364d9b
Kush Kumar <kuskumar0@publicisgroupe.net>
2019-04-11 19:12:00 +0530

    resolved

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -26,11 +26,7 @@ const Logo = (props) =>{
             }
         ]}
     />
-<<<<<<< HEAD
-    <div className="logo font-weight-light">Pioneers in Cleaning Accessory</div>
-=======
     <div className="logo font-weight-light font-italic">Pioneers in Cleaning Accessory</div>
->>>>>>> 3e00efa9c41fb6cc276c55e6318569f819746a28
         </div>
 
         
