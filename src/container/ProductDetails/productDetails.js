8b1db35f2290a8cd6856893b28d41160c9903cbe
shreya krishnan <kushdevui@gmail.com>
2019-06-12 23:54:34 +0530

    fixed

--- src/container/ProductDetails/productDetails.js
+++ src/container/ProductDetails/productDetails.js
@@ -11,6 +11,7 @@ import { connect } from 'react-redux'
 import "./product-details.scss";
 import Footer from "../../components/Footer/footer";
 import { addToCart } from '../../redux/actions/cartActions';
+import {getSingleProduct} from '../../redux/actions/getProductAction';
 import Slider from "react-slick";
 import FilterColors from "../FilterColors/filterColors"
 
@@ -28,12 +29,14 @@ class Product extends Component{
     }
 
     componentDidMount(){
+        //const {id,subCat,catName} = this.props.getSingleProduct;
+        //this.props.getSingleProduct(id,subCat,catName)
         const headers = {
             'Content-Type': 'application/json'
         }
        axios.post("https://fablasnode.herokuapp.com/products/getProductById",{
-        "id":this.props.match.params.product_id,
-        "sId":this.props.match.params.product_subCat
+        "id":this.props.getSingleProduct.id,
+        "sId":this.props.getSingleProduct.subCat
        }, {"headers": headers}).then(item=>{
             this.setState({
                 ProductData : item.data
@@ -71,7 +74,6 @@ class Product extends Component{
                             <FontAwesomeIcon className="mr-1" onClick={()=>{this.handleAddQuantity(item)}} icon={faArrowCircleUp } style={{color:'#999'}} size="sm" />
                                 1
                             <FontAwesomeIcon className="ml-1" onClick={()=>{this.handleSubtractQuantity(item)}}  icon={faArrowCircleDown } style={{color:'#999'}} size="sm" />
-                            
                         </div>
                         <div className="col-lg-3">
                             <div className="row">
@@ -119,7 +121,7 @@ class Product extends Component{
             return(
                 <div className="other-details">
                     <ul className="">
-                        <li className="mt-5">
+                        <li className="mt-3">
                                 <h4 onClick={(e)=>this.toggleDescription(e)}>
                                     <span className="pb-2 icon-palet"> <FontAwesomeIcon className="mr-1 p-1" icon={faShoppingCart }  style={{color:'white', background: 'black'}} size="lg" /> 
                                     {item.title}</span>
@@ -134,10 +136,10 @@ class Product extends Component{
 
 
     render(){
-
+   // console.log(this.props);
     //console.log(this.props.location.state.category);
    //console.log(this.state.ProductData[0]['images'][0]['path']);
-        const categoryName = this.props.location.state.category;
+        const categoryName = this.props.getSingleProduct.catName;
         var settings = {
             className: "slick-center",
             variableWidth: true,
@@ -149,18 +151,18 @@ class Product extends Component{
         };
         const productName = this.state.ProductData[0]?this.state.ProductData[0].productName:"";
         const imagePath =this.state.ProductData[0]?this.state.ProductData[0]['images'][0]['path'][0]:""
-        console.log(imagePath);
+        //console.log(imagePath);
         const ImagesArr = this.state.ProductData[0]?this.state.ProductData[0]['images'][0]['path']:"";
         const productUsage = this.state.ProductData[0]?this.state.ProductData[0].productUsage:"";
         const productFeature = this.state.ProductData[0]?this.state.ProductData[0].productFeature:"";
         var MainImage = `./assets/images/products/`+ categoryName+ `/`+ imagePath ;
-        console.log(MainImage);
+       // console.log(MainImage);
         var slides = [`./assets/images/products/`+ categoryName+ `/`+ productName + "-1.jpg",`./assets/images/products/`+ categoryName+ `/`+ productName + "-2.jpg",`./assets/images/products/`+ categoryName+ `/`+ productName + "-3.jpg"];
         const showImage = this.state.mainImage?this.state.mainImage:MainImage;
         return(
             <div>
                 <Header/>
-                <InnerHeader title="Product" subTitle="Product Details"/>
+                <InnerHeader  title="Product" subTitle="Product Details" subCatId={this.props.getSingleProduct.subCat} catName={this.props.getSingleProduct.catName}  pName={productName}/>
                 <div className="container mt-5 mb-5 product-details">
                     <div className="row mt-5">
                         <div className="col-lg-5">
@@ -198,17 +200,55 @@ class Product extends Component{
                         </div>
                         <div className="col-lg-7 product-details reduce-zindex">
                             <h4>{productName}</h4>
-                            <span>{this.state.ProductData.price}</span>
+                            <div>
+                                For cleaning pots, pans, ovens, Bar-be-que, grills, dishes, sinks
+                                tiles, bath tubs, machine parts, tools, tyers, mettalic equipments etc.
+                            </div>
+                            {/* <span>{this.state.ProductData.price}</span> */}
+                            <div className="text-red">Other Details</div>
+                            <ul className="other-details-extra">
+                                <li>
+                                    <div className="d-flex">
+                                        <span className="heading1">Length</span>
+                                        <span>10 CM</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="d-flex">
+                                        <span className="heading1">Breadth</span>
+                                        <span>10 CM</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="d-flex">
+                                        <span className="heading1">Weight</span>
+                                        <span>20 GM</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="d-flex">
+                                        <span className="heading1">Code</span>
+                                        <span>200CU</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="d-flex">
+                                        <span className="heading1">Material Used</span>
+                                        <span>Stain Less Steel</span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div>{this.state.ProductData.description}</div>
                             <div className="other-details">
                                 <ul className="">
-                                    <li className="mt-5">
+                                    <li className="mt-4">
                                         <h4 onClick={(e)=>this.toggleDescription(e)}>
                                             <span className="pb-2 icon-palet"> <FontAwesomeIcon className="mr-1 p-1" icon={faShoppingCart }  style={{color:'white', background: 'black'}} size="lg" /> 
                                             Usage</span>
                                             <span className="inner">{productUsage}</span>
                                         </h4>
                                     </li>
-                                    <li className="mt-5">
+                                    <li className="mt-4">
                                         <h4 onClick={(e)=>this.toggleDescription(e)}>
                                             <span className="pb-2 icon-palet"> <FontAwesomeIcon className="mr-1 p-1" icon={faShoppingCart }  style={{color:'white', background: 'black'}} size="lg" /> 
                                             Features</span>
@@ -217,16 +257,16 @@ class Product extends Component{
                                     </li>
                                 </ul>
                         </div>
+                            <button className="btn btn-danger">Get Quote</button>
                         </div>
                     </div>
-                    {
-                        this.state.type=="sell"?<div className="row mt-5">
-                        <div className="col-lg-12 mt-5">
+                    <div className="row mt-3">
+                        <div className="col-lg-12 mt-3">
                         <Tabs onSelect={(index, label) => console.log(label + ' selected')}>
-                            <Tab label="Description">
-                                {this.state.ProductData.fullDesc}
-                            </Tab>
-                            <Tab label="Details">
+                            {/* <Tab label="Description">
+                                {this.state.ProductData.description}
+                            </Tab> */}
+                            <Tab label="Review">
                                 <ul>
                                     {this.state.ProductData.details?this.state.ProductData.details.map(item=>{
                                         return <li>
@@ -236,13 +276,12 @@ class Product extends Component{
                                     }):""}
                                 </ul>
                             </Tab>
-                            <Tab label="Details">Reviews</Tab>
+                            {/* <Tab label="Review">Reviews</Tab> */}
                         </Tabs>
                         </div>
-                    </div>:""
-                    }
+                    </div>
                 </div>
-                {/* <div className="container related-products mb-5">
+                <div className="container related-products mb-5">
                     <div className="row mb-5 text-center">
                         <div className="col-lg-12">
                             <h3>Related Products</h3>
@@ -251,20 +290,20 @@ class Product extends Component{
                     </div>
                     <div className="row">
                         <div className="col-lg-3">
-                            <ProductItem/>
+                            {/* <ProductItem/> */}
                         </div>
                         <div className="col-lg-3">
-                            <ProductItem/>
+                            {/* <ProductItem/> */}
                         </div>
                         <div className="col-lg-3">
-                            <ProductItem/>
+                            {/* <ProductItem/> */}
                         </div>
                         <div className="col-lg-3">
-                            <ProductItem/>
+                            {/* <ProductItem/> */}
                         </div>
                     </div>
 
-                </div> */}
+                </div>
                 <Footer/>
             </div>
         )
@@ -273,12 +312,13 @@ class Product extends Component{
 const mapStateToProps = state =>{
     return {
         cartLength:state.cartReducer.addedItems.length,
+        getSingleProduct:state.getProductReducer.singleSelectedProduct,
         items: state.items
     }
 }
 const mapDispatchToProps= (dispatch)=>{
     return{
-        addToCart: (id)=>{dispatch(addToCart(id))}
+        addToCart: (id)=>{dispatch(addToCart(id))},
     }
 }
 
